b4a6bba093e132481708b8ab11a917a97a4580f9
cmangelo <christianangelo112@msn.com>
2020-06-09 16:50:47 -0700

    Update mongoose.ts

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
-export const connect = () => mongoose.connect('mongodb://127.0.0.1:27017/draft-api-ts', {
+// mongodb://127.0.0.1:27017/draft-api-ts
+export const connect = () => mongoose.connect('mongodb://heroku_pxcz10ss:dfnekm19hq8ko6791sa5e0hcgs@ds053140.mlab.com:53140/heroku_pxcz10ss', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
