1cfeac9246844ba005991867861aa197fd4daa8d
cmangelo <christianangelo112@msn.com>
2020-06-25 20:23:33 -0700

    Using environment variable

--- src/db/mongoose.ts
+++ src/db/mongoose.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
 // mongodb://127.0.0.1:27017/draft-api-ts
-export const connect = () => mongoose.connect('mongodb://heroku_pxcz10ss:dfnekm19hq8ko6791sa5e0hcgs@ds053140.mlab.com:53140/heroku_pxcz10ss', {
+// export const connect = () => mongoose.connect('mongodb://heroku_pxcz10ss:dfnekm19hq8ko6791sa5e0hcgs@ds053140.mlab.com:53140/heroku_pxcz10ss', {
+const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/draft-api-ts';
+export const connect = () => mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
